1d6c8e4a956cf6a622c6fe8531cce6a1f70afc41
Alexandre Pretyman <alexandre.pretyman@gmail.com>
2016-08-22 23:38:16 -0300

    :lipstick: main.cpp: fixed identation in initialize()

--- main.cpp
+++ main.cpp
@@ -61,10 +61,10 @@ bool initialize() {
 		std::cerr << "ERROR: could not start GLFW3\n";
 		return false;
 	}
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_DOUBLEBUFFER, GL_TRUE);
 	// Create a window of size 640x480 and with title "COMP371: Assignment 1"
 	window = glfwCreateWindow(800, 800, "COMP371: Assignment 1", NULL, NULL);
